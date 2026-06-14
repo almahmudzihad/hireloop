b3e17067a1e91fa59b59b93f226975e37f51c357
almahmudzihad <amzihad@gmail.com>
2026-06-14 15:50:24 +0600

    stripe setup for payment

--- src/app/jobs/[id]/apply/page.jsx
+++ src/app/jobs/[id]/apply/page.jsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 import JobApply from './JobApply';
 import { getApplicationsByApplicant } from '@/lib/api/applications';
+import { Linden_Hill } from 'next/font/google';
+import Link from 'next/link';
 
 const ApplyPage = async ({params}) => {
     const {id} = await params;
@@ -20,12 +22,18 @@ const ApplyPage = async ({params}) => {
     }
     const job = await getJobById(id);
     const application = await getApplicationsByApplicant(user.id);
+    const plan = {
+        name: 'free',
+        maxApplicationsPerMonth: 3,
+    }
   return (
     <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
         <h1 className="text-3xl font-bold tracking-tight mb-2">Apply to {job.jobTitle}</h1>
-        <h2 className="text-zinc-400 mb-6">You have applied to {application.length} jobs out of 3</h2>
+        <h2 className="text-zinc-400 mb-2">You have applied to {application.length} jobs out of {plan.maxApplicationsPerMonth} allowed per month</h2>
+        <h3 className="text-zinc-400 mb-2">Purchease a plan to increase your application limit</h3>
+        <Link href="/plans" className="text-zinc-400 hover:text-zinc-300">Pricing</Link>
         <div className="animate-in fade-in-50 duration-300">
-            <JobApply applicant={user} job={job} />
+            {application.length < plan.maxApplicationsPerMonth && <JobApply applicant={user} job={job} />}
         </div>
     </div>
   )
